b452ddff66bd2264df7a09ba8922d26dd3a517e3
Артур Цудик <arturcudik@MacBook-Air-Artur.local>
2022-06-25 22:29:26 +0200

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const positiveNamber = Math.abs(n);
+    const string = String(positiveNamber);
+    const splitReverse = string.split("");
+    const splitString = splitReverse[0];
+    const reverseArray = splitReverse.reverse();
+    const joinReverse = reverseArray.join("");
+
+    return Number(joinReverse);
+};
